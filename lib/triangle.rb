6a261753c293d9498a431d10220e4c5ce01fcf07
Jebediah Elliott <jebediahelliott@gmail.com>
2018-06-05 01:44:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,15 @@
 require 'pry'
 class Triangle
-  attr_accessor :side1, :side2, :side3
+  attr_accessor :side1, :side2, :side3, :sides
 
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
+    @sides = [side1, side2, side3]
+    if self.sides.any? { |s| s < 0  }
+      raise TriangleError
+    end
   end
 
   def isosceles?
